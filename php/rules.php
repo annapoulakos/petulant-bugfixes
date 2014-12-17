4270a354e1b783798ba0c696d61cd159c42f5016
James Poulakos <jimp@ebizresults.com>
2014-12-16 18:28:59 -0600

    Trim extraneous stuff before working on chaining rules.

--- php/rules.php
+++ php/rules.php
@@ -1,17 +1,40 @@
 <?php
-
 /** Simple Rules Engine **/
+class Factory {
+    protected static $rules = array();
+    public static function Rule ($rule) {
+        if (!isset(self::$rules[$rule])) {
+            self::$rules[$rule] = new Rule($rule);
+        }
 
-class Container {
+        return self::$rules[$rule];
+    }
+}
+
+class Rule {
+    public function __construct ($rule) {
+        $this->rule = $rule;
+        $this->value = null;
+
+        $this->condition = function ($context) {
+            return false;
+        };
+        $this->then = function ($context) {};
+    }
+    public function execute () {
+        if ($this->condition) {
+            $this->then;
+        }
+    }
+
     protected $_data = array();
     public function __set ($key, $value) {
         $this->_data[$key] = $value;
     }
     public function __get ($key) {
         if (!isset($this->_data[$key])) {
-            throw new Exception ("{$key} is not a valid key");
+            throw new Exception("{$key} is not a valid key.");
         }
-
         if (is_callable($this->_data[$key])) {
             return $this->_data[$key]($this);
         }
@@ -27,59 +50,3 @@ class Container {
         };
     }
 }
-
-class Factory {
-    protected static $rules = array();
-    public static function Rule ($rule, $value = null) {
-        if (!isset(self::$rules[$rule])) {
-            self::$rules[$rule] = new Rule($value);
-        }
-
-        return self::$rules[$rule];
-    }
-}
-
-class Rule extends Container {
-    public function __construct ($value) {
-        $this->value = $value;
-
-        $this->condition = function ($c) {
-            return false;
-        };
-        $this->then = function ($c) {
-            // Add default logging here?
-        };
-    }
-
-    public function execute () {
-        if ($this->condition) {
-            $this->then;
-        }
-    }
-}
-
-$v = 25;
-$rule = Factory::Rule('Less than or equal to 50', $v);
-
-$rule->condition = function ($context) {
-    return $context->less_than(50) or $context->equals(50);
-};
-
-$rule->then = function ($context) {
-    $context->value = 50;
-};
-
-$rule->execute();
-
-$v = 100;
-
-$rule2 = Factory::Rule('Greater than or equal to 50', $v);
-$rule2->condition = function ($context) {
-    return $context->greater_than(50) or $context->equals(50);
-};
-
-$rule2->then = function ($context) {
-    $context->value = 50;
-};
-
-$rule2->execute();
